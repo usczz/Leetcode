414d672a2dc10254e1523aa4271a68030329e26b
zhitao zhou <davidzhou0621@gmail.com>
2016-08-17 01:57:43 -0700

    Solve Reverse String Problem

---
+++ src/ReverseString.java
@@ -0,0 +1,28 @@
+/**
+ * Created by davidzhou on 8/17/16.
+ */
+public class ReverseString {
+    private void swap(char[] buffer, int left, int right) {
+        char tmp = buffer[left];
+        buffer[left] = buffer[right];
+        buffer[right] = tmp;
+    }
+
+    public String reverseString(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        char[] buffer= s.toCharArray();
+        int left = 0, right = buffer.length - 1;
+        while (left < right) {
+            swap(buffer, left++, right--);
+        }
+        return new String(buffer);
+    }
+
+    public static void main(String[] args) {
+        ReverseString obj = new ReverseString();
+        System.out.println(obj.reverseString("hello"));
+    }
+}
