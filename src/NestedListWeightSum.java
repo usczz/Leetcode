07b37ce8e1125e67708c87da2050489084467a73
zhitao zhou <davidzhou0621@gmail.com>
2016-08-18 23:09:54 -0700

    Leetcode 399 Nested List Weight Sum

--- src/NestedListWeightSum.java
+++ src/NestedListWeightSum.java
@@ -1,7 +1,25 @@
-package PACKAGE_NAME;
-
 /**
  * Created by davidzhou on 8/18/16.
  */
 public class NestedListWeightSum {
+    private int depthSum(NestedInteger nestedList, int current, int depth) {
+        if (nestedList.isInteger()) {
+            return current + nestedList.getInteger() * depth;
+        }
+        List<NestedInteger> sublist = nestedList.getList();
+        for (NestedInteger nl : sublist) {
+            current = depthSum(nl, current, depth + 1);
+        }
+        return current;
+    }
+    public int depthSum(List<NestedInteger> nestedList) {
+        if (nestedList == null || nestedList.size() == 0) {
+            return 0;
+        }
+        int sum = 0;
+        for (NestedInteger nl : nestedList) {
+            sum = depthSum(nl, sum, 1);
+        }
+        return sum;
+    }
 }
---
+++ tests/NestedListWeightSumTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by davidzhou on 8/18/16.
+ */
+public class NestedListWeightSumTest {
+
+}
