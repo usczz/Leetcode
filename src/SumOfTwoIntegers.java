0230e897ba8e178be94e2c740bd671b812bab227
zhitao zhou <davidzhou0621@gmail.com>
2016-09-23 22:48:34 -0700

    Leetcode 371 Sum of Two Integers

---
+++ src/SumOfTwoIntegers.java
@@ -0,0 +1,18 @@
+/**
+ * Created by davidzhou on 8/30/16.
+ */
+
+import java.util.*;
+
+// Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+// Bit Manipulation
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        int sum = a ^ b;
+        int carry = (a & b) << 1;
+        return getSum(sum, carry);
+    }
+}
