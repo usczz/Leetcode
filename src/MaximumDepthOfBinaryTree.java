24f5668418f7571e4204e55fe56420f772c4d3af
zhitao zhou <davidzhou0621@gmail.com>
2016-09-23 23:38:51 -0700

    Leetcode 104 Maximum Depth of Binary Tree

---
+++ src/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,14 @@
+/**
+ * Created by davidzhou on 9/23/16.
+ */
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
